97733f29e95f28d051c7d008146b75913cf82808
Venkateshchamakuri <154118073+Venkateshchamakuri@users.noreply.github.com>
2024-01-25 17:40:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "APP1" {
+resource "aws_instance" "APP2" {
   ami           = "ami-00952f27cf14db9cd"
   instance_type = "t2.micro"
   user_data     = file("${path.module}/App-install.sh") # file provisioners 
   tags = {
-    Name = "APP1"
+    Name = "APP2"
 
   }
 }
