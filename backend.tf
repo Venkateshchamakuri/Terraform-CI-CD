ab0d46fb6abca43ec8626253457b15430396206a
Venkateshchamakuri <154118073+Venkateshchamakuri@users.noreply.github.com>
2024-01-25 21:49:42 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "mentor1235"
+    bucket  = "venkat143"
     key     = "terraform.tfstate"
     region  = "ap-south-1" # Change this to your desired AWS region
     encrypt = true
